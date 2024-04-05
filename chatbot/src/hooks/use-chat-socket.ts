135067196aabe2ce45e4d0ab912f5fb0622bdd30
kishaan22 <kishanmewada2122@gmail.com>
2024-04-05 17:51:31 -0400

    final

--- chatbot/src/hooks/use-chat-socket.ts
+++ chatbot/src/hooks/use-chat-socket.ts
@@ -35,7 +35,7 @@ export const useChatSocket = ({
                     return oldData;
                 }
 
-                const newData = oldData.pages.mao((page: any) => {
+                const newData = oldData.pages.map((page: any) => {
                     return {
                         ...page,
                         items: page.items.map((item: MessageWithMemberWithProfile) => {
